8003da5d6f1a2f0cd456a169bce9e3046ef260d6
Akshat42 <akshatdivya03@gmail.com>
2024-09-29 15:55:31 +0530

    feat: footer mWeb view

--- app/lib/components/footer/footer.tsx
+++ app/lib/components/footer/footer.tsx
@@ -1,5 +1,52 @@
+import styles from "./footer.module.css";
+import FacebookIcon from "@assets/icons/facebook.svg";
+import InstaIcon from "@assets/icons/instagram.svg";
+import LinkedinIcon from "@assets/icons/linkedin.svg";
+import TwitterIcon from "@assets/icons/twitter.svg";
+import EWSLogo from "@assets/icons/ews-logo.svg";
+import GoToTop from "@assets/icons/to-top.svg";
+
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className={styles.contentContainer}>
+        <div className={styles.headerContainer}>
+          <h4>Quick Links</h4>
+          <GoToTop />
+        </div>
+        <ul className={styles.linkContainer}>
+          <li>
+            <span className={styles.link}>Home</span>
+          </li>
+          <li>
+            <span className={styles.link}>Programs</span>
+          </li>
+          <li>
+            <span className={styles.link}>Contact Us</span>
+          </li>
+        </ul>
+        <div className={styles.followUsContainer}>
+          <h4>Follow Us</h4>
+        </div>
+        <div className={styles.iconContainer}>
+          <FacebookIcon />
+          <LinkedinIcon />
+          <InstaIcon />
+          <TwitterIcon />
+        </div>
+        <div className={styles.logoContainer}>
+          <EWSLogo />
+          <div className={styles.logoTitle}>
+            <p>AI-Driven Environmental </p>
+            <p>Education for Everyone, Everywhere</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.copyrighContainer}>
+        <p>© {new Date().getFullYear()}. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
