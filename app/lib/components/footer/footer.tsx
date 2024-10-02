afb6730c5921a213903e40a4d699057bdf9ad527
Akshat42 <akshatdivya03@gmail.com>
2024-10-02 20:45:55 +0530

    fix: set footer max width

--- app/lib/components/footer/footer.tsx
+++ app/lib/components/footer/footer.tsx
@@ -9,50 +9,56 @@ import GoToTop from "@assets/icons/to-top.svg";
 const Footer: React.FC = () => {
     return (
         <footer>
-            <div className={styles.contentContainer}>
-                <div className={styles.rightContainer}>
-                    <div>
-                        <div className={styles.headerContainer}>
-                            <h4>Quick Links</h4>
+            <div className={styles.footerBg}>
+                <div className={styles.contentContainer}>
+                    <div className={styles.rightContainer}>
+                        <div>
+                            <div className={styles.headerContainer}>
+                                <h4>Quick Links</h4>
+                                <div className={styles.arrowMweb}>
+                                    <GoToTop/>
+                                </div>
+                            </div>
+                            <ul className={styles.linkContainer}>
+                                <li>
+                                    <span className={styles.link}>Home</span>
+                                </li>
+                                <li>
+                                    <span className={styles.link}>Programs</span>
+                                </li>
+                                <li>
+                                    <span className={styles.link}>Contact Us</span>
+                                </li>
+                            </ul>
                         </div>
-                        <ul className={styles.linkContainer}>
-                            <li>
-                                <span className={styles.link}>Home</span>
-                            </li>
-                            <li>
-                                <span className={styles.link}>Programs</span>
-                            </li>
-                            <li>
-                                <span className={styles.link}>Contact Us</span>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <div className={styles.followUsContainer}>
-                            <h4>Follow Us</h4>
+                        <div>
+                            <div className={styles.followUsContainer}>
+                                <h4>Follow Us</h4>
+                            </div>
+                            <div className={styles.iconContainer}>
+                                <FacebookIcon className={styles.icons}/>
+                                <LinkedinIcon/>
+                                <InstaIcon/>
+                                <TwitterIcon/>
+                            </div>
                         </div>
-                        <div className={styles.iconContainer}>
-                            <FacebookIcon className={styles.icons}/>
-                            <LinkedinIcon/>
-                            <InstaIcon/>
-                            <TwitterIcon/>
+                        <div className={styles.arrowDesktop}>
+                            <GoToTop/>
                         </div>
                     </div>
-                    <div className={styles.arrowDesktop}>
-                        <GoToTop/>
-                    </div>
-                </div>
-                <div className={styles.logoContainer}>
-                    <EWSLogo/>
-                    <div className={styles.logoTitle}>
-                        <p>AI-Driven Environmental </p>
-                        <p>Education for Everyone, Everywhere</p>
+                    <div className={styles.logoContainer}>
+                        <EWSLogo/>
+                        <div className={styles.logoTitle}>
+                            <p>AI-Driven Environmental </p>
+                            <p>Education for Everyone, Everywhere</p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className={styles.copyrightContainer}>
                 <p>© {new Date().getFullYear()}. All rights reserved.</p>
             </div>
+
         </footer>
     );
 };
