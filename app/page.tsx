c6a91269b4a0b98a9af234bbcc492976f3a24c47
Akshat42 <akshatdivya03@gmail.com>
2024-10-06 13:51:32 +0530

    feat: contact us component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "@lib/components/hero/hero";
 import AboutUs from "@lib/components/about-us/about-us";
+import ContactUs from "@lib/components/contact-us/contact-us";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <ContactUs />
       <AboutUs />
     </>
   );
