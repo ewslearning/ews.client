12a2f6badacd41c7c3019350c90357a33cc54bbe
Akshat42 <akshatdivya03@gmail.com>
2024-10-08 11:24:18 +0530

    feat: our partner mobile view

---
+++ app/lib/components/our-partners/our-partners.tsx
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import classes from "./our-partners.module.css";
+import WellsFargoIcon from "@assets/icons/wells-fargo.svg?url";
+
+const OurPartners: React.FC = () => {
+  return (
+    <section className={classes.ourPartnersContainer}>
+      <div className={classes.headerContainer}>
+        <h4 className={classes.subHeaderText}>
+          Lorem IPSUM Lorem IPSUM Lorem IPSUM
+        </h4>
+        <h1 className={classes.headerText}>Our Partners</h1>
+      </div>
+      <div className={classes.companiesContainer}>
+        <div className={classes.companyContent}>
+          <Image
+            src={WellsFargoIcon}
+            width={42}
+            height={42}
+            alt="wells fargo icon"
+          />
+          <p className={classes.companyText}>Company Name</p>
+        </div>
+        <div className={classes.companyContent}>
+          <Image
+            src={WellsFargoIcon}
+            width={42}
+            height={42}
+            alt="wells fargo icon"
+          />
+          <p className={classes.companyText}>Company Name</p>
+        </div>
+        <div className={classes.companyContent}>
+          <Image
+            src={WellsFargoIcon}
+            width={42}
+            height={42}
+            alt="wells fargo icon"
+          />
+          <p className={classes.companyText}>Company Name</p>
+        </div>
+        <div className={classes.companyContent}>
+          <Image
+            src={WellsFargoIcon}
+            width={42}
+            height={42}
+            alt="wells fargo icon"
+          />
+          <p className={classes.companyText}>Company Name</p>
+        </div>
+        <div className={classes.companyContent}>
+          <Image
+            src={WellsFargoIcon}
+            width={42}
+            height={42}
+            alt="wells fargo icon"
+          />
+          <p className={classes.companyText}>Company Name</p>
+        </div>
+        <div className={classes.companyContent}>
+          <Image
+            src={WellsFargoIcon}
+            width={42}
+            height={42}
+            alt="wells fargo icon"
+          />
+          <p className={classes.companyText}>Company Name</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default OurPartners;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@lib/components/hero/hero";
 import AboutUs from "@lib/components/about-us/about-us";
 import ContactUs from "@lib/components/contact-us/contact-us";
 import KeyServices from "@lib/components/key-services/key-services";
+import OurPartners from "@lib/components/our-partners/our-partners";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <KeyServices />
       <ContactUs />
       <AboutUs />
+      <OurPartners />
     </>
   );
 }
