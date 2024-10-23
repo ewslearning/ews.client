18d8f43ccbb9f1855a4032cdb7c5354a32222e80
Akshat42 <akshatdivya03@gmail.com>
2024-10-23 22:27:58 +0530

    feat: form skeleton mweb

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import ContactUs from "@lib/components/contact-us/contact-us";
 import KeyServices from "@lib/components/key-services/key-services";
 import OurPartners from "@lib/components/our-partners/our-partners";
 import Testimonials from "@lib/components/testimonials/testimonials";
+import Newsletter from "@lib/components/newsletter/newsletter";
 
 export default function Home() {
   return (
@@ -12,8 +13,9 @@ export default function Home() {
       <Hero />
       <AboutUs />
       <KeyServices />
-      <Testimonials/>
+      <Testimonials />
       <ContactUs />
+      <Newsletter />
       <OurPartners />
     </>
   );
