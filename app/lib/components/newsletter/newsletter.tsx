09badbf458824c4afb569d9d6c2979f363c81f79
Akshat42 <akshatdivya03@gmail.com>
2024-10-27 15:40:02 +0530

    feat: ews input

--- app/lib/components/newsletter/newsletter.tsx
+++ app/lib/components/newsletter/newsletter.tsx
@@ -4,12 +4,14 @@ import classes from "./newsletter.module.css";
 export const Newsletter = () => {
   return (
     <article className={classes.newsletterContainer}>
-      <h1 className={classes.header}>Stay Updated with Our Newsletter</h1>
-      <p className={classes.description}>
-        Sign up to receive the latest news, updates, and exclusive insights from
-        our team. Be the first to know about our programs, certifications, and
-        collaborations!
-      </p>
+      <div className={classes.headerContainer}>
+        <h1 className={classes.header}>Stay Updated with Our Newsletter</h1>
+        <p className={classes.description}>
+          Sign up to receive the latest news, updates, and exclusive insights
+          from our team. Be the first to know about our programs,
+          certifications, and collaborations!
+        </p>
+      </div>
       <form>
         <p>Form Here</p>
       </form>
