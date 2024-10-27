99e6dd8abead29a08de44276dd2366a3c571a391
Akshat42 <akshatdivya03@gmail.com>
2024-10-27 16:29:19 +0530

    feat newsletter input form

--- app/lib/components/common/button/button.tsx
+++ app/lib/components/common/button/button.tsx
@@ -2,17 +2,22 @@ import classes from "./button.module.css";
 
 type ButtonVariants = "primary" | "secondary";
 
-interface ButtonProps {
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariants;
   children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ variant = "primary", children }) => {
+const Button: React.FC<ButtonProps> = ({
+  variant = "primary",
+  children,
+  ...props
+}) => {
   return (
     <button
       className={`${classes.button} ${
         variant === "primary" ? classes.primary : classes.secondary
       }`}
+      {...props}
     >
       {children}
     </button>
--- app/lib/components/newsletter/newsletter.tsx
+++ app/lib/components/newsletter/newsletter.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import Button from "@components/common/button/button";
 import classes from "./newsletter.module.css";
+import Input from "@components/common/input/input";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  orgName: string;
+};
 
 export const Newsletter = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
     <article className={classes.newsletterContainer}>
       <div className={classes.headerContainer}>
@@ -12,17 +30,51 @@ export const Newsletter = () => {
           certifications, and collaborations!
         </p>
       </div>
-      <form>
-        <p>Form Here</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={classes.inputcontainer}>
+          <Input
+            name="firstName"
+            label="First Name"
+            placeholder="Enter your first name"
+            register={register}
+            error={errors.firstName}
+          />
+
+          <Input
+            name="lastName"
+            label="Last Name"
+            placeholder="Enter your first name"
+            register={register}
+            error={errors.firstName}
+          />
+
+          <Input
+            name="email"
+            label="Email Address"
+            placeholder="Enter your email"
+            register={register}
+            error={errors.firstName}
+          />
+
+          <Input
+            name="orgName"
+            label="Organization Name"
+            placeholder="You Organization / Company Name"
+            register={register}
+            error={errors.firstName}
+          />
+        </div>
+
+        <p className={classes.disclamer}>
+          By Clicking Submit you agree I agree to receive email communications
+          from [Your Organization Name].I understand that I can unsubscribe at
+          any time.
+        </p>
+
+        <Button typeof="submit" variant="secondary">
+          Submit
+        </Button>
       </form>
-
-      <p className={classes.disclamer}>
-        By Clicking Submit you agree I agree to receive email communications
-        from [Your Organization Name].I understand that I can unsubscribe at any
-        time.
-      </p>
-
-      <Button variant="secondary">Submit</Button>
     </article>
   );
 };
