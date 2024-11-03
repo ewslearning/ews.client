614af585a24701948df37da84c2e407f84cbb2a8
Abhishek Kumar <abhi.code101@gmail.com>
2024-11-04 02:05:48 +0530

    Added suspense in courseDescription

--- app/courseoverview/page.tsx
+++ app/courseoverview/page.tsx
@@ -2,12 +2,15 @@
 import ContactUs from "@app/home/components/contact-us/contact-us";
 import CourseDescription from "./components/course-description";
 import Hero from "./components/hero/hero";
+import { Suspense } from "react";
 
 export default function Courseoverview() {
   return (
     <>
       <Hero/>
-      <CourseDescription/>
+      <Suspense fallback={<div>Loading...</div>}>
+    <CourseDescription />
+  </Suspense>
       <ContactUs/>
     </>
   );
