b1167ac5bd60701a1ad72afbee6e5502bef510ec
Abhishek Kumar <abhi.code101@gmail.com>
2024-11-04 01:57:57 +0530

    Renamed course overview

--- app/courseoverview/page.tsx
+++ app/courseoverview/page.tsx
@@ -3,7 +3,7 @@ import ContactUs from "@app/home/components/contact-us/contact-us";
 import CourseDescription from "./components/course-description";
 import Hero from "./components/hero/hero";
 
-export default function CourseOverview() {
+export default function Courseoverview() {
   return (
     <>
       <Hero/>
