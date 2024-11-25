0199e5e37997355c415479fc06e654c50344874e
Abhishek Kumar <abhi.code101@gmail.com>
2024-11-25 17:47:48 +0530

    Added metadata

--- app/page.tsx
+++ app/page.tsx
@@ -6,43 +6,11 @@ import KeyServices from "@home/components/key-services/key-services";
 import OurPartners from "@home/components/our-partners/our-partners";
 import Testimonials from "@home/components/testimonials/testimonials";
 import Newsletter from "@home/components/newsletter/newsletter";
-import Head from "next/head";
+import type { Metadata } from 'next'
 
 export default function Home() {
-  function addProductJsonLd() {
-    return {
-      __html: `{
-      "@context": "https://schema.org/",
-      "@type": "Product",
-      "name": "EWS Learning",
-      "image": [],
-      "description": "Empowering Sustainable Futures through Environmental Education & Corporate Sustainability.",
-      "sku": "0446310786",
-      "mpn": "925872",
-      "brand": {
-        "@type": "Brand",
-        "name": "ACME"
-      },
-    }
-  `,
-    };
-  }
   return (
     <>
-    <Head>
-      <title>EWS Learning</title>
-      <meta name="keywords" content="ews, ewsLearning, ews learning, ews learn" />
-      <meta   name="title"  key="title"  property="og:title" content="My Amazing Website" />
-        <meta  name="description"  key="desc" property="og:description" content="Empowering Sustainable Futures through Environmental Education & Corporate Sustainability." />
-        <meta property="og:url" content="https://ewslearning.com" />
-        <meta property="og:type" content="website" />
-
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={addProductJsonLd()}
-          key="product-jsonld"
-        />
-      </Head>
       <Hero />
       <AboutUs />
       <KeyServices />
@@ -53,3 +21,11 @@ export default function Home() {
     </>
   );
 }
+
+export const metadata: Metadata = {
+  metadataBase:  new URL('https://ewslearning.com'),
+  title: 'EWS Learning',
+  description: "Empowering Sustainable Futures through Environmental Education & Corporate Sustainability",
+  keywords: "ews learning, ewslearning, ews, EWS Learning, ews learn"
+}
+ 
