0178fd062a9fda5736af4ba9ca3a8cfe4fce8326
Abhishek Kumar <abhi.code101@gmail.com>
2025-06-30 01:04:40 +0530

    Add support for GPT crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,18 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: "*",
         allow: "/",
       },
+      {
+        userAgent: "ChatGPT-User",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ChatGPT-User/1.0",
+        allow: "/",
+      },
     ],
     sitemap: "https://ewslearning.com/sitemap.xml",
   };
